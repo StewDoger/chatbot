6eebbe610b37f6a1901b5a9c80b37478009f0174
Antonius Sakti Wiradinata <wira@xashlez.com>
2024-12-12 03:56:51 +0700

    add new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Chatbot from './Components/Chatbot';
+import Home from './Components/Home';
+import About from './Components/About';
+import Help from './Components/Help';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const location = useLocation(); // Mendapatkan lokasi saat ini
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/help">Help</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <TransitionGroup>
+                <CSSTransition key={location.key} classNames="fade" timeout={400}>
+                    <Routes location={location}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/help" element={<Help />} />
+                        <Route path="/chatbot" element={<Chatbot />} />
+                    </Routes>
+                </CSSTransition>
+            </TransitionGroup>
+        </div>
+    );
 }
 
-export default App;
+// Bungkus App dengan Router
+const MainApp = () => (
+    <Router>
+        <App />
+    </Router>
+);
+
+export default MainApp;
